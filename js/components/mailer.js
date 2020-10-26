ed7ee27ac1daed97b365de0cae7c3844b5658066
theMightyT <halvas_trevor@halvas.ca>
2020-10-26 15:44:21 -0400

    adding some comments

--- js/components/mailer.js
+++ js/components/mailer.js
@@ -1,20 +1,25 @@
+// could wrap all of this in a class or an object and expand on the API to include all of the front-end API interactions (GET, PUT, POST for CMS etc)
+
+// SendMail would be just one member / property in that object
+
 async function SendMail(targetForm) {
     // mail stuff goes here
     let formData = new FormData(targetForm);
+
+    // TODO: examine the method and incrementally build out the fetch call. currently it assumes POST
 
     let result = await fetch(`./includes/${targetForm.getAttribute("action")}`, {
         method: targetForm.method,
         headers: {
             "Content-type":"application/x-www-form-urlencoded"
         },
-        // could include headers here but they're breaking my Fetch call
+        // TODO: a GET request doesn't use body, so this member should be conditional
         body: formData
     }).then(response => {
         if (response.status !== 200) {
             throw new Error(`Mail submission failed: ${response.status}`);
 
-            // could examine status code here and return it as part of an error object to the .catch
-            // handler in the SendMail thenable chain in main.js
+            // could examine status code here and return it as part of an error object to the .catch handler in the SendMail then-able chain in main.js
         }
 
         return response;
