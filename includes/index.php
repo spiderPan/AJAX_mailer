aee8937d2b1b017bb9c1f0714f4b2b7df277eab0
theMightyT <halvas_trevor@halvas.ca>
2020-10-26 11:29:18 -0400

    adding php message

--- includes/index.php
+++ includes/index.php
@@ -1,4 +1,10 @@
 <?php
     // hi pan. Feel free to change this however you like
     // this is just a placeholder file
-    echo json_encode(["message" => "this is a test message"]);
+
+    if (isset($POST['firstname'])) {
+        echo json_encode(["message" => "this is a test message"]);
+    } else {
+        echo json_encode(["message" => "form submission failed"]);
+    }
+    
